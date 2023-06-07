3f3965807482f567d55e7945467cda769407c86c
Paul Ferraron <paul.ferraron@gmail.com>
2023-06-07 22:01:49 +0200

    feat: add legal mentions controller, route and template with temporary data

--- src/Controller/LegalMentionsController.php
+++ src/Controller/LegalMentionsController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class LegalMentionsController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/legal/mentions', name: 'app_legal_mentions')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('legal_mentions/index.html.twig', [
+            'controller_name' => 'LegalMentionsController',
         ]);
     }
 }
